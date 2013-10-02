27fccc60ab67ce67d4623eebdb9d1e8be5fc92b3
Marcio Gomes Goncalves <marcio28@gmail.com>
2013-10-01 21:39:21 -0300

    Speaker e Contact

---
+++ eventex/core/admin.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from django.contrib import admin
+from eventex.core.models import Speaker, Contact
+
+class ContactInLine(admin.TabularInline):
+    model = Contact
+    extra = 1
+
+class SpeakerAdmin(admin.ModelAdmin):
+    inlines = [ContactInline,]
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Speaker, SpeakerAdmin)
+
